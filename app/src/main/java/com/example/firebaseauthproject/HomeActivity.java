18f4667282dc0c796e51a046f80846513f85c548
Hardcore Programmer <30619162+gamebit34@users.noreply.github.com>
2019-10-07 14:46:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/firebaseauthproject/HomeActivity.java
@@ -0,0 +1,42 @@
+package com.example.firebaseauthproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.support.design.widget.BottomNavigationView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_favorites:
+                                Toast.makeText(getApplicationContext(), "Favorites", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.action_home:
+                                Toast.makeText(getApplicationContext(), "Home", Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.action_profile:
+                                Intent inentProfile = new Intent(HomeActivity.this, ProfileActivity.class);
+                                startActivity(inentProfile);
+                                break;
+                        }
+                        return true;
+                    }
+                });
+    }
+}
